387e3afd120ebe08ab7d1218b8ce465ed0648e61
sanjay krishna <sanjay.info.g@gmail.com>
2025-08-01 07:16:06 +0530

    Updated backendfile

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "demo-resources"
-    storage_account_name = "techtutorialswithpiyush"
+    resource_group_name  = "backend"
+    storage_account_name = "backendstorageaccountsg"
     container_name       = "prod-tfstate"
     key                  = "prod.terraform.tfstate"
   }
